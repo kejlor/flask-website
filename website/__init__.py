3950a55b5413ab26b65bb1c33e4954104fea745a
kejlor <woj450@gmail.com>
2021-11-03 13:00:05 +0100

    Deleted unnecessary file. Created app building file. Added two base models. One for user and one for todo entry. Created database for models.

---
+++ website/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret key'
+
+    return app
---
+++ website/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy.sql import func
+
+from . import db
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    description = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(150))
+    email = db.Column(db.String(150))
+    password = db.Column(db.String(150))
+    todos = db.relationship('Todo')
